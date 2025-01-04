8ed6e8d1f3ddc9ec0a5df507a1b1718d10c22a5b
Vignesh Rao <166179991+dormant-user@users.noreply.github.com>
2025-01-04 16:22:02 -0600

    Add default libraries with `shutil` during startup

--- pyarchitecture/config.py
+++ pyarchitecture/config.py
@@ -1,4 +1,5 @@
 import platform
+import shutil
 
 try:
     from enum import StrEnum
@@ -38,16 +39,18 @@ if OPERATING_SYSTEM not in (
 def default_mem_lib():
     """Returns the default memory library dedicated to linux and macOS."""
     return dict(
-        linux="/proc/meminfo", darwin="/usr/sbin/sysctl", windows=""  # placeholder
+        linux="/proc/meminfo",
+        darwin=shutil.which("sysctl") or "/usr/sbin/sysctl",
+        windows="",  # placeholder
     )
 
 
 def default_disk_lib():
     """Returns the default disks' library dedicated to each supported operating system."""
     return dict(
-        linux="/usr/bin/lsblk",
-        darwin="/usr/sbin/diskutil",
-        windows="C:\\Program Files\\PowerShell\\7\\pwsh.exe",
+        linux=shutil.which("lsblk") or "/usr/bin/lsblk",
+        darwin=shutil.which("diskutil") or "/usr/sbin/diskutil",
+        windows=shutil.which("pwsh") or "C:\\Program Files\\PowerShell\\7\\pwsh.exe",
     )
 
 
@@ -55,15 +58,15 @@ def default_cpu_lib():
     """Returns the default processor library dedicated to each supported operating system."""
     return dict(
         linux="/proc/cpuinfo",
-        darwin="/usr/sbin/sysctl",
-        windows="C:\\Windows\\System32\\wbem\\wmic.exe",
+        darwin=shutil.which("sysctl") or "/usr/sbin/sysctl",
+        windows=shutil.which("wmic") or "C:\\Windows\\System32\\wbem\\wmic.exe",
     )
 
 
 def default_gpu_lib():
     """Returns the default GPU library dedicated to each supported operating system."""
     return dict(
-        linux="/usr/bin/lspci",
-        darwin="/usr/sbin/system_profiler",
-        windows="C:\\Windows\\System32\\wbem\\wmic.exe",
+        linux=shutil.which("lspci") or "/usr/bin/lspci",
+        darwin=shutil.which("system_profiler") or "/usr/sbin/system_profiler",
+        windows=shutil.which("wmic") or "C:\\Windows\\System32\\wbem\\wmic.exe",
     )
